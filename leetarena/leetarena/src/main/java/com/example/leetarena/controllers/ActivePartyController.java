f571bc1cbd9bdcdb2678a1080cf196e19a4adeb8
ericpapazzz <ericrendong@gmail.com>
2025-08-24 13:46:17 -0700

    Change Active party EndPoint

--- leetarena/leetarena/src/main/java/com/example/leetarena/controllers/ActivePartyController.java
+++ leetarena/leetarena/src/main/java/com/example/leetarena/controllers/ActivePartyController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
-@RequestMapping("/Partys")
+@RequestMapping("/partys")
 @CrossOrigin(origins = "http://localhost:8080")
 public class ActivePartyController {
 
