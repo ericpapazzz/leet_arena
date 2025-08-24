7783a29e6294c1c09cdf2efe3f0d169166fc0158
rhamses <rhamses09.rot@gmail.com>
2025-08-23 23:18:10 -0700

    Add Product

---
+++ leetarena/leetarena/src/main/java/models/Product.java
@@ -0,0 +1,34 @@
+package models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer product_id;
+
+    @Column(name = "product_name")
+    private String product_name;
+
+    @Column(name = "product_price")
+    private Integer product_price;
+
+    @Column(name = "product_img")
+    private String product_img;
+
+    @Column(name = "product_description")
+    private String product_description;
+
+    @Column(name = "product_tag")
+    private String product_tag;
+}
---
+++ leetarena/leetarena/src/main/java/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+import models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByProductName(String productName); //search product by name
+}
