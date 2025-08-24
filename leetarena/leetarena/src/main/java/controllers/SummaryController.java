37c67edacf6819c278e1e619807bb5d6a3c2990e
Emiliano0X1 <egp2506@gmail.com>
2025-08-24 00:01:46 -0700

    Party Summary Endpoint completed

---
+++ leetarena/leetarena/src/main/java/controllers/SummaryController.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import models.Summary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.SummaryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/summary")
+public class SummaryController {
+
+    @Autowired
+    private final SummaryService summaryService;
+
+    public SummaryController(SummaryService summaryService) {
+        this.summaryService = summaryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Summary>> getAllSummarys(){
+        return ResponseEntity.ok(summaryService.getAllSummarys());
+    }
+
+    @GetMapping("/{sumamry_id}")
+    public ResponseEntity<Summary> getSummaryById(@PathVariable("sumamry_id") int sumamry_id){
+        return ResponseEntity.ok(summaryService.getSummaryById(sumamry_id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Summary> createSummary(@RequestBody Summary summary){
+        return ResponseEntity.ok(summaryService.addSummary(summary));
+    }
+
+    @PatchMapping("/edit_des/{summary_id}")
+    public ResponseEntity<Summary> updateSummaryDescription(@PathVariable("summary_id") int summary_id, @RequestParam() String description){
+        return ResponseEntity.ok(summaryService.updateSummaryDescription(summary_id, description));
+    }
+
+    @DeleteMapping("/delete/{summary_id}")
+    public void deleteSummary(@PathVariable("summary_id") int summary_id){
+        summaryService.deleteSummary(summary_id);
+    }
+
+}
--- leetarena/leetarena/src/main/java/repositories/RecordRepository.java
+++ leetarena/leetarena/src/main/java/repositories/RecordRepository.java
@@ -4,9 +4,11 @@ import models.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Integer> {
 
     @Query(value = "SELECT r FROM Record r WHERE r.user.user_id = :user_id")
