f00b0498d5e5e8a63dc00366a44b6c7a186e80ee
Emiliano0X1 <egp2506@gmail.com>
2025-08-23 21:46:07 -0700

    Records endpoints

---
+++ leetarena/leetarena/src/main/java/controllers/RecordController.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import models.Record;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.RecordService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/record")
+public class RecordController {
+
+    @Autowired
+    private final RecordService recordService;
+
+    public RecordController(RecordService recordService) {
+        this.recordService = recordService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Record>> getRecordAllRecords() {
+        return ResponseEntity.ok(recordService.getAllRecords());
+    }
+
+    @GetMapping("/{record_id}")
+    public ResponseEntity<Record> getRecordById(@PathVariable("record_id") int record_id) {
+        return ResponseEntity.ok(recordService.getRecordById(record_id));
+    }
+
+    @GetMapping("/byuser/{user_id}")
+    public ResponseEntity<List<Record>> getRecordsByUserId(@PathVariable("user_id") int user_id) {
+        return ResponseEntity.ok(recordService.getRecordsByUser(user_id));
+    }
+
+    @PostMapping("/create")
+    public void addRecord(@RequestBody Record newRecord) {
+        recordService.createRecord(newRecord);
+    }
+
+    @DeleteMapping("/delete/{record_id}")
+    public void deleteRecordById(@PathVariable("record_id") int record_id) {
+        recordService.deleteRecord(record_id);
+    }
+
+    @PutMapping("/update/{record_id}")
+    public void updateRecord(@PathVariable("record_id") int record_id,@RequestBody Record updatedRecord) {
+        recordService.updateRecord(record_id, updatedRecord);
+    }
+
+}
---
+++ leetarena/leetarena/src/main/java/models/Record.java
@@ -0,0 +1,31 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table( name = "records")
+public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int records_id;
+
+    @Column(name = "ranking")
+    private String ranking;
+
+    @Column(name = "end_time")
+    private LocalDateTime end_time;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    /*
+    @ManyToOne()
+    @JoinColumn(name = "active_party_id)
+    private Active_Party active_party;
+     */
+}
